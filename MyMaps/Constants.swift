3027adb61f4de6c2e3868725ee0f95cb3b05af7d
Sanjeev Suman <SanjeevSuman@zc-sanjeevs-macbook-pro.local>
2018-02-15 13:13:48 +0530

    initial commit

---
+++ MyMaps/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  MyMaps
+//
+//  Created by Sanjeev Suman on 15/02/18.
+//  Copyright © 2018 Sanjeev Suman. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    struct APIKey {
+        static let google = "AIzaSyApuaORdjys0T7-6C0A07wsT3fMfRIJMmQ"
+    }
+}
---
+++ Podfile
@@ -0,0 +1,9 @@
+platform :ios, '9.0'
+use_frameworks!
+
+target 'MyMaps' do
+
+  pod 'GoogleMaps'
+  pod 'GooglePlaces'
+
+end
